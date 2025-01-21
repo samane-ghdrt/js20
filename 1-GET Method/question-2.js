29a6b9066c176a35d1e6be279cc3f54a963c2587
samane <samane.ghodrati@gmail.com>
2025-01-21 13:39:36 -0800

    add

--- 1-GET Method/question-2.js
+++ 1-GET Method/question-2.js
@@ -5,3 +5,17 @@
 */
 
 // ! Answer
+async function getData() {
+    const url = "https://jsonplaceholder.typicode.com/users";
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+  
+      const json = await response.json();
+      console.log(json);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
