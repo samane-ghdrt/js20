740d61b1e2ff90e533c9cfaaae0c8fd3903d4d13
samane ghodrati <samane.ghodrati@gmail.com>
2025-01-25 19:00:40 -0900

    add

--- 4-DELETE Method/question-6.js
+++ 4-DELETE Method/question-6.js
@@ -11,6 +11,49 @@
 
 
 //*End of story :)
+const article = document.querySelector("article");
+article.style.display = "flex";
+article.style.justifyContent = "center";
+article.style.gap = "30px";
+article.style.flexWrap = "wrap";
+const url="https://67936e815eae7e5c4d8e9f86.mockapi.io/users";
+async function cards() {
+    try {
+        const data= await axios(url);
+        const data1=await data.data;  
+        data1.forEach(element => {
+          makeUserData(element)
+         });
+         return data1;
+    } catch (error) {
+       throw console.log(error);
+        
+    }
+    }
+    cards();
+    function makeUserData (element){
+    const cardEl=document.createElement('div');
+    article.append(cardEl);
+    const nameEl=document.createElement("h3");
+    const lastNameEl=document.createElement("h3");
+    // const phoneEl=document.createElement("h3");
+    cardEl.append(nameEl);
+    cardEl.append(lastNameEl);
+    // cardEl.append(phoneEl);
+    nameEl.textContent=element.firstname;
+    lastNameEl.textContent=element.lastname;
+    // phoneEl.textContent=element.phone;
+    cardEl.style.display = "flex";
+    cardEl.style.justifyContent = "center";
+    cardEl.style.flexDirection = "column-reverse";
+    cardEl.style.alignItems = "center";
+    
+    cardEl.style.border="1px solid black";
+    cardEl.style.borderRadius = "10px";
+    cardEl.style.padding = "10px";
+    cardEl.style.width = "180px";
+    }
+    // ---------------------------------------delete
 
 const formDelete = document.querySelector("#delete");
 
